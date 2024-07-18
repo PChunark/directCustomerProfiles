790ca11ac4c85b83cb365a821947ea62e62d8a28
PChunark <puttipongchunark@gmail.com>
2024-07-18 16:23:41 +0700

    add plot time series

--- code/getData.R
+++ code/getData.R
@@ -3,26 +3,39 @@ library(fs)
 library(readxl)
 library(openxlsx)
 
+# Rearrange file names each month
 file_path <- 
   tibble(filename = list.files("rawData/",
-                                          pattern = "*2023",
+                                          pattern = "Coincident",
                                           full.names = F)) %>% 
   mutate(filepath = paste0("rawData/", filename)) %>% 
   separate(col = filename, into = c("name1","file"), sep = "_") %>% 
-  separate(col = file, into = c("month","filetype"), sep = " ") %>% 
+  separate(col = file, into = c("month","filetype"), sep = " ") %>%
+  separate(col = filetype, into = c("year","filetypes"), sep = ".x") %>% 
   mutate(month = factor(month, levels = month.name)) %>% 
-  arrange(month) %>% 
+  arrange(year, month) %>% 
   pull(filepath)
 
+# get data from a "Data Direct" sheet
 data1 <-
   file_path %>%
   map(function(path){
     read_excel(path, 
                sheet = "Data Direct",
-               skip = 3,
-               # range = cell_cols("A:AE"),
-               col_names = T,
-               .name_repair = "minimal")  #Choose column 1 to 30 
-    
-  } %>% drop_na()
+               skip = 3
+               )  
+  }%>% 
+  select(1:9) %>% 
+  drop_na()
   )
+
+# Combine monthly data to yearly data
+data2 <-
+  data1 %>% reduce(full_join, by = c(colnames(.)))
+
+plot.ts(ts(data2, frequency = 48))
+plot.ts(log(ts(data2, frequency = 48)))
+
+data3 <-
+  data2 %>% 
+  pi
